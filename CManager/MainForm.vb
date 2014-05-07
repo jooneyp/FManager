e8c5c2e04c0b82de283e75e3a201049448ad6267
jooneyp <parkjydev@gmail.com>
2014-05-07 11:02:14 +0900

    import ver 0.5
    엑셀 처리기능 내부구현 완료. 디버그 필요. 엑셀에 대한 접근 불가.

--- CManager/MainForm.vb
+++ CManager/MainForm.vb
@@ -19,4 +19,8 @@
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
         PrintForm.Show()
     End Sub
+
+    Private Sub btn__Click(sender As Object, e As EventArgs) Handles btn_.Click
+        Import.Show()
+    End Sub
 End Class
