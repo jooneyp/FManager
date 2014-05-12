d9ae0dd94c63d7eccc1647321d3e9581377be5c9
Jooney <parkjydev@gmail.com>
2014-05-12 20:13:30 +0900

    Revert e8c5c2e..6ce5afe
    This rolls back to commit e8c5c2e04c0b82de283e75e3a201049448ad6267.

---
+++ CManager/Import.designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Import
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.lbl_stat = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(70, 201)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 30
+        Me.DataGridView1.Size = New System.Drawing.Size(764, 200)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'lbl_stat
+        '
+        Me.lbl_stat.AutoSize = True
+        Me.lbl_stat.Location = New System.Drawing.Point(343, 89)
+        Me.lbl_stat.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_stat.Name = "lbl_stat"
+        Me.lbl_stat.Size = New System.Drawing.Size(244, 25)
+        Me.lbl_stat.TabIndex = 1
+        Me.lbl_stat.Text = "데이터 불러오는 중.."
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(243, 500)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(586, 500)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(411, 443)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(149, 89)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 25)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Label4"
+        '
+        'Import
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(928, 664)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl_stat)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Import"
+        Me.Text = "Import"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents lbl_stat As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+End Class
--- CManager/MainForm.Designer.vb
+++ CManager/MainForm.Designer.vb
@@ -27,44 +27,41 @@ Partial Class MainForm
         Me.Button_Deal = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_ = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button_ClientMod
         '
-        Me.Button_ClientMod.Location = New System.Drawing.Point(431, 28)
-        Me.Button_ClientMod.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button_ClientMod.Location = New System.Drawing.Point(560, 37)
         Me.Button_ClientMod.Name = "Button_ClientMod"
-        Me.Button_ClientMod.Size = New System.Drawing.Size(94, 77)
+        Me.Button_ClientMod.Size = New System.Drawing.Size(122, 103)
         Me.Button_ClientMod.TabIndex = 6
         Me.Button_ClientMod.Text = "업체" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "관리"
         Me.Button_ClientMod.UseVisualStyleBackColor = True
         '
         'Button_UserMod
         '
-        Me.Button_UserMod.Location = New System.Drawing.Point(302, 28)
-        Me.Button_UserMod.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button_UserMod.Location = New System.Drawing.Point(393, 37)
         Me.Button_UserMod.Name = "Button_UserMod"
-        Me.Button_UserMod.Size = New System.Drawing.Size(94, 77)
+        Me.Button_UserMod.Size = New System.Drawing.Size(122, 103)
         Me.Button_UserMod.TabIndex = 5
         Me.Button_UserMod.Text = "사용자" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "관리"
         Me.Button_UserMod.UseVisualStyleBackColor = True
         '
         'Button_Deal
         '
-        Me.Button_Deal.Location = New System.Drawing.Point(42, 28)
-        Me.Button_Deal.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button_Deal.Location = New System.Drawing.Point(55, 37)
         Me.Button_Deal.Name = "Button_Deal"
-        Me.Button_Deal.Size = New System.Drawing.Size(94, 77)
+        Me.Button_Deal.Size = New System.Drawing.Size(122, 103)
         Me.Button_Deal.TabIndex = 4
         Me.Button_Deal.Text = "거래내역 입력"
         Me.Button_Deal.UseVisualStyleBackColor = True
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(172, 28)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Location = New System.Drawing.Point(224, 37)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(94, 77)
+        Me.Button1.Size = New System.Drawing.Size(122, 103)
         Me.Button1.TabIndex = 7
         Me.Button1.Text = "거래내역 출력"
         Me.Button1.UseVisualStyleBackColor = True
@@ -72,25 +69,33 @@ Partial Class MainForm
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(393, 120)
+        Me.Label1.Location = New System.Drawing.Point(511, 160)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(132, 18)
+        Me.Label1.Size = New System.Drawing.Size(187, 25)
         Me.Label1.TabIndex = 8
         Me.Label1.Text = "For Office 2010"
         '
+        'btn_
+        '
+        Me.btn_.Location = New System.Drawing.Point(722, 37)
+        Me.btn_.Name = "btn_"
+        Me.btn_.Size = New System.Drawing.Size(122, 103)
+        Me.btn_.TabIndex = 9
+        Me.btn_.Text = "업체" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "관리"
+        Me.btn_.UseVisualStyleBackColor = True
+        '
         'MainForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(568, 150)
+        Me.ClientSize = New System.Drawing.Size(882, 183)
+        Me.Controls.Add(Me.btn_)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Button_ClientMod)
         Me.Controls.Add(Me.Button_UserMod)
         Me.Controls.Add(Me.Button_Deal)
-        Me.Margin = New System.Windows.Forms.Padding(2)
-        Me.MaximumSize = New System.Drawing.Size(590, 206)
-        Me.MinimumSize = New System.Drawing.Size(590, 206)
         Me.Name = "MainForm"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -102,5 +107,6 @@ End Sub
     Friend WithEvents Button_Deal As System.Windows.Forms.Button
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btn_ As System.Windows.Forms.Button
 
 End Class
