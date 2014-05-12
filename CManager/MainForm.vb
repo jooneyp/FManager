6ce5afee329fe6bcfcfc798f49dce818fc94451e
jooneyp <parkjydev@gmail.com>
2014-05-12 19:53:32 +0900

    Revert "import ver 0.5"
    This reverts commit e8c5c2e04c0b82de283e75e3a201049448ad6267.

--- CManager/MainForm.vb
+++ CManager/MainForm.vb
@@ -19,8 +19,4 @@
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
         PrintForm.Show()
     End Sub
-
-    Private Sub btn__Click(sender As Object, e As EventArgs) Handles btn_.Click
-        Import.Show()
-    End Sub
 End Class
