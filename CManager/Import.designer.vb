6342b786439bd97ed680785c6aea8705abb6ccde
jooneyp <parkjydev@gmail.com>
2014-05-17 19:49:22 +0900

    1차완성
    5월 17일 문자보내드린 날

--- CManager/Import.designer.vb
+++ CManager/Import.designer.vb
@@ -22,94 +22,30 @@ Partial Class Import
     '코드 편집기를 사용하여 수정하지 마십시오.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.lbl_stat = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(70, 201)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.Height = 30
-        Me.DataGridView1.Size = New System.Drawing.Size(764, 200)
-        Me.DataGridView1.TabIndex = 0
-        '
-        'lbl_stat
-        '
-        Me.lbl_stat.AutoSize = True
-        Me.lbl_stat.Location = New System.Drawing.Point(343, 89)
-        Me.lbl_stat.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lbl_stat.Name = "lbl_stat"
-        Me.lbl_stat.Size = New System.Drawing.Size(244, 25)
-        Me.lbl_stat.TabIndex = 1
-        Me.lbl_stat.Text = "데이터 불러오는 중.."
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(243, 500)
+        Me.Label1.Location = New System.Drawing.Point(70, 35)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(86, 25)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(586, 500)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(86, 25)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Label2"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(411, 443)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(86, 25)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "Label3"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(149, 89)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(86, 25)
-        Me.Label4.TabIndex = 5
-        Me.Label4.Text = "Label4"
+        Me.Label1.Size = New System.Drawing.Size(125, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "데이터 입력중입니다.."
         '
         'Import
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(928, 664)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
+        Me.ClientSize = New System.Drawing.Size(270, 85)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.lbl_stat)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Import"
         Me.Text = "Import"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents lbl_stat As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Label4 As System.Windows.Forms.Label
 End Class
--- CManager/Import.vb
+++ CManager/Import.vb
@@ -1,21 +1,29 @@
 ﻿Imports System.Data.OleDb
 Imports Excel = Microsoft.Office.Interop.Excel
 
+
 Public Class Import
+
     Private myConn As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=.\SourceDB.mdb")
 
     Private Sub Import_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For d As Integer = 0 To 1
+            Label1.Show()
+        Next
         Dim ExcelApp As Excel.Application
         Dim WorkB As Excel.Workbook
         Dim WorkS As Excel.Worksheet
+        Dim cmdText As String
 
         ExcelApp = New Excel.Application
         ExcelApp.Visible = False
         Dim ofd As New OpenFileDialog
         If ofd.ShowDialog = DialogResult.OK Then
             WorkB = ExcelApp.Workbooks.Open(ofd.FileName)
+        ElseIf ofd.ShowDialog = DialogResult.Abort Then
+            MessageBox.Show("취소하였습니다.")
+
         End If
-        Label4.Text = ofd.FileName
 
         Dim sCount As Integer
         Dim cCount As Integer
@@ -24,9 +32,10 @@ Public Class Import
         Dim i As Integer
         Dim j As Integer
         Dim commentText As String
-        Dim data As String() = {}
+        Dim data() As String = {"", "", "", "", "", ""}
 
-        Dim cmdText = "INSERT INTO [deal] (d_client, d_user, d_date, d_cost, d_tons, d_qty) VALUES (?, ?, ?, ?, ?, ?)"
+        myConn.Open()
+
 
         cUser = 0
         cCount = 0
@@ -34,43 +43,83 @@ Public Class Import
         Do Until sCount = ExcelApp.Worksheets.Count
             index = 2
             WorkS = ExcelApp.Sheets(sCount)
-            Label3.Text = sCount
             Do Until WorkS.Cells(index, 5).Value = ""
                 i = 3
-                Label1.Text = i
-                Do Until WorkS.Cells(index + i, 1).Value = ""
+                Do Until WorkS.Cells(index + i, 1).Value = "" Or i > 8
                     For j = 3 To 33
-                        'add client name
-                        data(0) = WorkS.Cells(index, 5).Value
-                        Label2.Text = 0
-                        'add user name
-                        data(1) = WorkS.Cells(index + i, 1).Value
-                        Label2.Text = 1
-                        If WorkS.Cells(index + 1, j).Value <> "" Then
+                        If WorkS.Cells(index + i, j).Value <> Nothing Then
+
+                            'add client name
+                            data(0) = WorkS.Cells(index, 5).Value
+                            Debug.WriteLine(data(0))
+                            'add user name
+                            data(1) = WorkS.Cells(index + i, 1).Value
+                            Debug.WriteLine(data(1))
                             'add date
-                            data(2) = WorkS.Cells(2, 36).Value + "-" + WorkS.Cells(index + 2, j).Value
-                            Label2.Text = 2
-                            If WorkS.Cells(index + 1, j).Comment.Text <> "" Then
-                                commentText = WorkS.Cells(index + 1, j).Comment.Text
+                            data(2) = ExcelApp.Sheets(1).Cells(2, 36).Value & "-" & WorkS.Cells(index + 2, j).Value
+                            Debug.WriteLine(data(2))
+                            Try
+                                commentText = WorkS.Cells(index + i, j).Comment.Text
                                 'add cost&tons
+                                Debug.WriteLine(commentText)
                                 If commentText = "a" Then
-                                    'data(cUser)(3) = cost
-                                    'data(cUser)(4) = tons
+                                    data(3) = "a"
+                                    data(4) = "a"
                                 ElseIf commentText = "b" Then
-
+                                    data(3) = "b"
+                                    data(4) = "b"
                                 ElseIf commentText = "c" Then
-
+                                    data(3) = ""
+                                    data(4) = ""
+                                ElseIf commentText = "d" Then
+                                    data(3) = "b"
+                                    data(4) = ""
+                                ElseIf commentText = "e" Then
+                                    data(3) = ""
+                                    data(4) = ""
+                                ElseIf commentText = "f" Then
+                                    data(3) = ""
+                                    data(4) = ""
+                                ElseIf commentText = "g" Then
+                                    data(3) = ""
+                                    data(4) = ""
+                                ElseIf commentText = "h" Then
+                                    data(3) = ""
+                                    data(4) = ""
+                                ElseIf commentText = "i" Then
+                                    data(3) = ""
+                                    data(4) = ""
+                                ElseIf commentText = "j" Then
+                                    data(3) = ""
+                                    data(4) = ""
+                                ElseIf commentText = "k" Then
+                                    data(3) = ""
+                                    data(4) = ""
                                 End If
-                            Else
+                                Debug.WriteLine(data(3))
+                                Debug.WriteLine(data(4))
+                            Catch ex As Exception
                                 data(3) = "40000"
-                                Label2.Text = 3
+                                Debug.WriteLine(data(3))
                                 data(4) = "(7t)"
-                                Label2.Text = 4
-                            End If
+                                Debug.WriteLine(data(4))
+                            End Try
                             'add qty
-                            data(5) = WorkS.Cells(index + 1, j).Value
-                            Label2.Text = 5
-                            DataGridView1.Rows.Add(data)
+                            data(5) = WorkS.Cells(index + i, j).Value
+                            Debug.WriteLine(data(5))
+                            'End If
+
+                            cmdText = "INSERT INTO [deal] (d_client, d_user, d_date, d_cost, d_tons, d_qty) VALUES (?, ?, ?, ?, ?, ?)"
+                            Dim myCmd = New OleDbCommand(cmdText, myConn)
+                            myCmd.Parameters.AddWithValue("@d_client", data(0))
+                            myCmd.Parameters.AddWithValue("@d_user", data(1))
+                            myCmd.Parameters.AddWithValue("@d_date", data(2))
+                            myCmd.Parameters.AddWithValue("@d_cost", data(3))
+                            myCmd.Parameters.AddWithValue("@d_tons", data(4))
+                            myCmd.Parameters.AddWithValue("@d_qty", data(5))
+                            myCmd.ExecuteNonQuery()
+
+                            Debug.WriteLine("")
                         End If
                     Next
                     cUser += 1
@@ -81,16 +130,7 @@ Public Class Import
             Loop
             sCount += 1
         Loop
-        lbl_stat.Text = "데이터베이스 입력 중.."
-        For i = 0 To cUser
-            Dim myCmd = New OleDbCommand(cmdText, myConn)
-            myCmd.Parameters.AddWithValue("@d_client", data(0))
-            myCmd.Parameters.AddWithValue("@d_user", data(1))
-            myCmd.Parameters.AddWithValue("@d_date", data(2))
-            myCmd.Parameters.AddWithValue("@d_cost", data(3))
-            myCmd.Parameters.AddWithValue("@d_tons", data(4))
-            myCmd.Parameters.AddWithValue("@d_qty", data(5))
-            myCmd.ExecuteNonQuery()
-        Next
+        myConn.Close()
+        Label1.Text = "데이터 입력이 완료되었습니다."
     End Sub
 End Class
